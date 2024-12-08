af0b3afaa4a21c828a744d9abf903251c9abc547
mihalypal <palijan.09@gmail.com>
2024-12-08 01:19:06 +0100

    little expressJS & ejs project for devops

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Set EJS as the template engine
+app.set('view engine', 'ejs');
+
+// Serve static files from the "public" directory
+app.use(express.static('public'));
+
+// Define a route for the home page
+app.get('/', (req, res) => {
+  res.render('index', { title: 'DevOps alapanyag' });
+});
+
+// Define a route for the game page
+app.get('/game', (req, res) => {
+    res.render('game', { title: 'Tic-Tac-Toe' });
+  });
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
